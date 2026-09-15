4f1f15c050badc47159d7425bb5b5a9174852f69
mariacuartas30 <mariasalomeyepescuartas@gmail.com>
2026-09-15 12:56:48 -0500

    Mobs

--- ciclos.py
+++
@@ -1,8 +0,0 @@
-for x in range (5):
-    print("Numero: ", x)
-    
-amigos = ["Carlos", "Pedro", "Anderson"]
-for amigo in amigos:
-    letras = len(amigo)
-    print("Hola, ", amigo, " tienes ", letras, " letras.")  
-    
